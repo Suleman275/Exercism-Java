04a50929ff114d10997973adbaedea4b2f2c54d3
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-13 19:39:00 +0300

    solved isogram

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,27 @@
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
 
+        for (int i = 0; i < phrase.length(); i++) {
+            char l = phrase.charAt(i);
+            Character letter = Character.toUpperCase(l);
+            if (letter == '-' || letter == ' ') {
+               continue;
+            }
+            for (int j = 0; j < phrase.length(); j++) {
+               if (i == j) {
+                  continue;
+               }
+               if (phrase.charAt(j) == '-' || phrase.charAt(j) == ' ') {
+                  continue;
+               }
+               if (letter == Character.toUpperCase(phrase.charAt(j))) {
+                  return false;
+               }
+            }
+         }
+        
+        return true;
+    }
 }

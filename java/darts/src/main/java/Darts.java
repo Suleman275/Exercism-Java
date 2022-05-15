3b0b0d8ab0a170dba4ec8d02dfdec9fbf1beb197
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-15 08:39:57 +0300

    darts solution

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,11 +1,25 @@
 class Darts {
+    double distance = 0;
 
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        distance = Math.sqrt((x*x) + (y*y));
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (distance > 10) {
+            return 0;
+        }
+        else if (distance > 5) {
+            return 1;
+        }
+        else if (distance > 1) {
+            return 5;
+        }
+        else {
+            return 10;
+        }
     }
 
 }

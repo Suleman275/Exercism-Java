189697cf208e8f77290cced48621c3f53a451d82
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-16 11:23:01 +0300

    solved gigasecond

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -2,16 +2,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Gigasecond {
+    LocalDateTime moment;
+
     public Gigasecond(LocalDate moment) {
         // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-        moment.plusSeconds
+        this.moment = moment.atStartOfDay();
+        this.moment = this.moment.plusSeconds(1000000000);
     }
 
     public Gigasecond(LocalDateTime moment) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.moment = moment.plusSeconds(1000000000);
     }
 
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return moment;
     }
 }

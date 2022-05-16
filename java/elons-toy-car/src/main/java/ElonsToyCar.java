72ff96e28c73824c76e3619f885b55f38addf3bd
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-16 22:36:26 +0300

    solved exercise

--- java/elons-toy-car/src/main/java/ElonsToyCar.java
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -1,20 +1,40 @@
 public class ElonsToyCar {
     int battery;
     int meters;
-    
+
+    ElonsToyCar() {
+        battery = 100;
+        meters = 0;
+    }
+
     public static ElonsToyCar buy() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.buy()  method");
+        //throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.buy()  method");
+        return new ElonsToyCar();
     }
 
     public String distanceDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.distanceDisplay()  method");
+        // throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.distanceDisplay()  method");
+        return "Driven " + meters + " meters";
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.batteryDisplay()  method");
+        // throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.batteryDisplay()  method");
+        if (battery > 0) {
+            return "Battery at " + battery + "%";
+        }
+        else {
+            return "Battery empty";
+        }
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.drive()  method");
+        // throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.drive()  method");
+        if (battery > 0) {
+            this.meters += 20;
+            this.battery -= 1;
+        }
+        else {
+            System.out.println("Battery empty");
+        }
     }
 }

6f65ce9948203cffd6c0db0e791126a7475e92e3
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-16 14:38:21 +0300

    solved scrabble

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,42 @@
 class Scrabble {
+    String word;
 
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.word = word.toUpperCase();
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        int score = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            if (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'L' || letter == 'N' || letter == 'R' || letter == 'S' || letter == 'T') {
+                score += 1;
+            }
+            else if (letter == 'D' || letter == 'G') {
+                score += 2;
+            }
+            else if (letter == 'B' || letter == 'C' || letter == 'M' || letter == 'P') {
+                score += 3;
+            }
+            else if (letter == 'F' || letter == 'H' || letter == 'V' || letter == 'Y' || letter == 'W') {
+                score += 4;
+            }
+            else if (letter == 'K') {
+                score += 5;
+            }
+            else if (letter == 'J' || letter == 'X') {
+                score += 8;
+            }
+            else {
+                score += 10;
+            }
+        }
+
+        return score;
     }
 
 }

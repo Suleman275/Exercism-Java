1200461bc2867912ad1f47aa26bb81cf3e285894
Muhammad Suleman <m.suleman275@gmail.com>
2022-05-16 11:35:12 +0300

    solved exercise

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,27 @@
 class DifferenceOfSquaresCalculator {
+    int squareOfSum = 0;
+    int sumOfSquare = 0;
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (int i = 1; i <= input; i ++) {
+            squareOfSum += i;
+        }
+        return squareOfSum*squareOfSum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        for (int i = 1; i <= input; i ++) {
+            sumOfSquare += i*i;
+        }
+        return sumOfSquare;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
     }
 
 }
